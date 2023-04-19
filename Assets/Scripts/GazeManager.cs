998d017ff57885b86bec14d71c36dcce4c9db794
Luca <s0581820@htw-berlin.de>
2023-04-19 10:36:12 +0200

    Use Mouse As Gaze
    Mouse can now be used as gaze, left click is clising the eyes

--- Assets/Scripts/GazeManager.cs
+++ Assets/Scripts/GazeManager.cs
@@ -14,32 +14,33 @@ public class GazeManager : MonoBehaviour
     [SerializeField] float timeTillTelekinesis = 5;
     [SerializeField] float releaseDelayTime = 2;
 
-    [MinMaxSlider(0f, 50f)]
-    [SerializeField] Vector2 followSpeed;
+    [MinMaxSlider(0f, 50f)] [SerializeField]
+    Vector2 followSpeed;
+
     [SerializeField] float impactDistance = 5;
-    
+
     [SerializeField] bool debug;
     [SerializeField] bool useMouseAsGaze;
 
     GameObject currentLookingAt, currentAttachedObject;
-    
+
     Rigidbody attachedRb;
 
     Image indicatorFill;
-    
+
     float currentBlinkDuration;
     float currentGazeDuration;
     float distanceToGaze;
-    
+
     Vector2 objectPosOnScreen;
     Vector2 directionToTarget;
-    
+
     void Start()
     {
         TobiiAPI.Start(new TobiiSettings());
 
         indicatorFill = gazeIndicator.GetChild(0).GetComponent<Image>();
-        
+
         currentGazeDuration = 0;
     }
 
@@ -48,11 +49,11 @@ public class GazeManager : MonoBehaviour
         gazeIndicator.position = useMouseAsGaze ? Input.mousePosition : TobiiAPI.GetGazePoint().Screen;
 
         DetectObjectSwitch();
-        
+
         BlinkDetection();
 
         ObjectMovement();
-        
+
         if (currentAttachedObject)
         {
             RectTransform rt = radiusIndicator;
@@ -63,9 +64,9 @@ public class GazeManager : MonoBehaviour
 
     void DetectObjectSwitch()
     {
-        if(currentAttachedObject)
+        if (currentAttachedObject)
             return;
-        
+
         GameObject focusedObject = GetFocusedObject();
 
         if (focusedObject)
@@ -80,7 +81,7 @@ public class GazeManager : MonoBehaviour
 
             currentGazeDuration += Time.deltaTime;
             indicatorFill.fillAmount = currentGazeDuration / timeTillTelekinesis;
-            
+
             // Attach new Object
             if (currentGazeDuration >= timeTillTelekinesis)
             {
@@ -88,7 +89,7 @@ public class GazeManager : MonoBehaviour
                 currentGazeDuration = 0;
             }
         }
-        
+
         else
         {
             currentLookingAt = null;
@@ -101,21 +102,27 @@ public class GazeManager : MonoBehaviour
     {
         if (useMouseAsGaze)
         {
-            Camera.main.ScreenPointToRay(Input.mousePosition, vec)
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.GetComponent<GazeAware>())
+            {
+                return hit.collider.gameObject;
+            }
+
+            return null;
         }
-        else
-        {
-            return TobiiAPI.GetFocusedObject();
-        }
+
+        return TobiiAPI.GetFocusedObject();
     }
 
     void ObjectMovement()
     {
-        if(!currentAttachedObject)
+        if (!currentAttachedObject)
             return;
-        
+
         CalculateGazeData();
-        
+
         //--------------DEBUGGING
         if (debug)
         {
@@ -132,35 +139,36 @@ public class GazeManager : MonoBehaviour
 
     void Attach(GameObject _objToAttach)
     {
-        if(debug)
+        if (debug)
             Debug.Log("Attach!");
-        
+
         currentAttachedObject = _objToAttach;
         attachedRb = currentAttachedObject.GetComponent<Rigidbody>();
         attachedRb.useGravity = false;
-        
+
         radiusIndicator.gameObject.SetActive(true);
     }
-    
+
     void Detach()
     {
-        if(debug)
+        if (debug)
             Debug.Log("Detach!");
-        
+
         attachedRb.useGravity = true;
         attachedRb = null;
         currentAttachedObject = null;
-        
+
         radiusIndicator.gameObject.SetActive(false);
     }
-    
+
     void CalculateGazeData()
     {
-        Vector3 targetPos = TobiiAPI.GetGazePoint().Screen;
-        
+        Vector3 targetPos = useMouseAsGaze ? Input.mousePosition : TobiiAPI.GetGazePoint().Screen;
+
         // Gaze Direction
         objectPosOnScreen = Camera.main.WorldToScreenPoint(currentAttachedObject.transform.position);
-        directionToTarget = new Vector2(targetPos.x - objectPosOnScreen.x, targetPos.y - objectPosOnScreen.y).normalized;
+        directionToTarget = new Vector2(targetPos.x - objectPosOnScreen.x, targetPos.y - objectPosOnScreen.y)
+            .normalized;
 
         // Gaze Distance
         distanceToGaze = Vector2.Distance(objectPosOnScreen, targetPos);
@@ -169,16 +177,16 @@ public class GazeManager : MonoBehaviour
     void MoveInGazeDirection()
     {
         float currentFollowSpeed = Mathf.Lerp(followSpeed.x, followSpeed.y, distanceToGaze / impactDistance);
-        
+
         attachedRb.transform.position += ((Vector3)directionToTarget * currentFollowSpeed * Time.deltaTime);
     }
 
     void BlinkDetection()
     {
-        if(!currentAttachedObject)
+        if (!currentAttachedObject)
             return;
-        
-        bool gazeFound = TobiiAPI.GetGazePoint().IsRecent();
+
+        bool gazeFound = useMouseAsGaze ? !Input.GetMouseButton(0) : TobiiAPI.GetGazePoint().IsRecent();
 
         if (!gazeFound)
         {
@@ -186,11 +194,11 @@ public class GazeManager : MonoBehaviour
 
             if (currentBlinkDuration >= blinkThreshold)
             {
-                if(debug)
+                if (debug)
                     Debug.Log("Drop Object!");
-                
+
                 currentBlinkDuration = 0;
-                
+
                 Detach();
             }
         }
@@ -199,4 +207,5 @@ public class GazeManager : MonoBehaviour
             currentBlinkDuration = 0;
     }
 }
+
 
