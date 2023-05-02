fe305ab0d5ca96a18f88fa2b318bc1b2e8ddc7e2
Leonie Straßer <leonie.strasser@yahoo.de>
2023-05-02 16:22:55 +0200

    jau

--- Assets/Scripts/ColorChanger/ColorChanger.cs
+++ Assets/Scripts/ColorChanger/ColorChanger.cs
@@ -11,37 +11,21 @@ public class ColorChanger : MonoBehaviour
 
     [BoxGroup("Materialgroups to change Color")] [SerializeField] CustomizableMaterial[] ColorChangeMatsOfType;
 
-    [Button]
-    void TestLodeColorsFromPalette()
-    {
-        // für jedes Material das CustomizableMaterial x hat to folgendes
-        foreach (var mat in ColorChangeMatsOfType[0].materials)
-        {
-            //Check jeden Value von X´s Colorvalue Liste 
-            foreach (var myColorVal in ColorChangeMatsOfType[0].colorValues)
-            {
-                string _nameToCompare = myColorVal.valueName;
-                // Wenn die _currentPalette einen ColorValue mit dem selben string hat, setze hier im Colorvalue die Farbe von _currentPallettes entsprechendem ColorValue ein
-                foreach (var set in currentSetup.allColorValueSets)
-                {
-                    // Geh jedes Set des scriptable Objects durch
-                    foreach (var colorVal in set.allColorValues)
-                    {
-                        if (colorVal.valueName == _nameToCompare)
-                        {
-                            myColorVal.color = colorVal.color;
-                        }
 
-                    }
-                }
-            }
-        }
+    private void Start()
+    {
+
     }
+
+
+
 
 
     [Button]
     public void SetRandomColorsInScene()
     {
+        ClearAllInstanceMaterialLists();
+        SetAllMaterialInstances();
         SetRandomColorSet();
         LodeColorsFromPalette(currentSetup);
     }
@@ -57,23 +41,30 @@ public class ColorChanger : MonoBehaviour
         // Für jedes ColorValueSetForOneMaterial
         foreach (var matType in ColorChangeMatsOfType)
         {
+            int typeIndex = matType.index;
+
             // für jedes Material das ColorValueSetForOneMaterial x hat do folgendes -- Denn jedes Material braucht das selebe Color Update
-            foreach (var mat in ColorChangeMatsOfType[0].materials)
+            foreach (var matInstance in matType.MeshRendererInstancesInScene)
             {
                 //Check jeden Value der Colorvalue Liste im ColorValueSetForOneMaterial
-                foreach (var myColorVal in ColorChangeMatsOfType[0].colorValues)
+                foreach (var myColorVal in matType.colorValues)
                 {
                     string _nameToCompare = myColorVal.valueName;
                     // Wenn die _currentPalette einen ColorValue mit dem selben string hat, setze hier im Colorvalue des materials die Farbe von _currentPallettes entsprechendem ColorValue ein
                     foreach (var set in _currentPalette.allColorValueSets)
                     {
-                        // Geh jedes Set des scriptable Objects durch -- Die Sets existieren nur um die Farben einfacher einzustellen
-                        foreach (var colorVal in set.allColorValues)
+                        int setIndex = set.index;
+
+                        if (typeIndex == setIndex)
                         {
-                            if (colorVal.valueName == _nameToCompare)
+                            // Geh das Set des scriptable Objects durch -- Die Sets existieren nur um die Farben einfacher einzustellen
+                            foreach (var colorVal in set.allColorValues)
                             {
-                                myColorVal.color = colorVal.color;
-                                SetNewColor(mat, _nameToCompare, myColorVal.color);
+                                if ((colorVal.valueName == _nameToCompare))
+                                {
+                                    myColorVal.color = colorVal.color;
+                                    SetNewColor(matInstance.sharedMaterial, _nameToCompare, myColorVal.color);
+                                }
                             }
                         }
                     }
@@ -84,7 +75,50 @@ public class ColorChanger : MonoBehaviour
 
     void SetNewColor(Material _material, string _changeColorName, Color _newColor)
     {
-        _material.SetColor(_changeColorName, _newColor);
+        _material.SetColor(Shader.PropertyToID(_changeColorName), _newColor);
     }
 
+    void SetAllMaterialInstances()
+    {
+        MeshRenderer[] allRenderers = FindObjectsOfType<MeshRenderer>();
+        foreach (var type in ColorChangeMatsOfType)
+        {
+            foreach (var mat in type.materials)
+            {
+                bool foundInstance = false;
+
+                // Find all instances of the material in the scene
+                foreach (var renderer in allRenderers)
+                {
+                    if (renderer.sharedMaterial == mat)
+                    {
+                        type.MeshRendererInstancesInScene.Add(renderer);
+                        foundInstance = true;
+                    }
+                }
+
+                if (!foundInstance)
+                {
+                    Debug.LogWarning("No instances of material " + mat.name + " were found in the scene.");
+                }
+            }
+            Debug.Log("Material type " + type.materialType + " has found " + type.MeshRendererInstancesInScene.Count + " instance materials in the scene!");
+        }
+    }
+
+    void ClearAllInstanceMaterialLists()
+    {
+        foreach (var type in ColorChangeMatsOfType)
+        {
+            type.MeshRendererInstancesInScene.Clear();
+        }
+    }
+
+    [Button]
+    void DEBUG()
+    {
+        Debug.Log("Steinmaterialzahl ist " + ColorChangeMatsOfType[0].MeshRendererInstancesInScene.Count);
+    }
 }
+
+
--- Assets/Scripts/ColorChanger/CustomizableMaterial.cs
+++ Assets/Scripts/ColorChanger/CustomizableMaterial.cs
@@ -6,8 +6,31 @@ using UnityEngine;
 public class CustomizableMaterial
 {
     public string materialType;
+    public int index;
     public Material[] materials;
     public ColorValue[] colorValues;
+
+    private List<MeshRenderer> meshRendererInstancesInScene;
+    public List<MeshRenderer> MeshRendererInstancesInScene
+    {
+        get
+        {
+            if (meshRendererInstancesInScene == null)
+            {
+                meshRendererInstancesInScene = new List<MeshRenderer>();
+            }
+            return meshRendererInstancesInScene;
+        }
+
+        set
+        {
+            if (meshRendererInstancesInScene == null)
+            {
+                meshRendererInstancesInScene = new List<MeshRenderer>();
+            }
+            meshRendererInstancesInScene = value;
+        }
+    }
 
 
 }
