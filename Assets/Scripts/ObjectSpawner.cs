40a89d6692c8944211b7223a9b1778012332c73c
Luca <s0581820@htw-berlin.de>
2023-05-02 16:25:10 +0200

    SpawnImprovements
    Objects dont bug into each other anymore. Respawned objects dont spawn above win zones.

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using DG.Tweening;
 using NaughtyAttributes;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Quaternion = UnityEngine.Quaternion;
 using Random = UnityEngine.Random;
+using Vector3 = UnityEngine.Vector3;
 
 public class ObjectSpawner : MonoBehaviour
 {
@@ -46,7 +49,10 @@ public class ObjectSpawner : MonoBehaviour
         
         for (int i = 0; i < Random.Range(winZoneAmount.x, winZoneAmount.y + 1); i++)
         {
-            Vector3 winZonePosition = RandomPointInZone(winSpawnZone.bounds, 2.5f);
+            Vector3 winZonePosition = RandomPointInZone(winSpawnZone.bounds, 1f);
+
+            if (winZonePosition.magnitude > 1000f)
+                winZonePosition = winSpawnZone.bounds.center;
 
             if (addedHeight + winZonePosition.y > maxCombinedWinZoneHeight)
             {
@@ -63,17 +69,15 @@ public class ObjectSpawner : MonoBehaviour
         LevelManager.instance.RegisterWinZones();
     }
 
-    Vector3 RandomPointInZone( Bounds _bounds, float radius = 1.5f)
+    Vector3 RandomPointInZone(Bounds _bounds, float _radius = 1.5f)
     {
-        var bounds = objectSpawnZone.bounds;
-
         Vector3 spawnPoint = RandomSpawnPoint(_bounds);
 
         // Check for collisions within an increasing sphere until a valid point is found
         int attempts = 0;
         while (attempts < maxPositionFindAttempts)
         {
-            Collider[] colliders = Physics.OverlapSphere(spawnPoint, radius, LayerMask.NameToLayer("SpawnZone"));
+            Collider[] colliders = Physics.OverlapSphere(spawnPoint, _radius, LayerMask.NameToLayer("SpawnZone"));
             
             if (colliders.Length == 0)
             {
@@ -86,7 +90,8 @@ public class ObjectSpawner : MonoBehaviour
         }
 
         Debug.LogWarning("Failed to find a valid spawn point after " + maxPositionFindAttempts + " attempts.");
-        return bounds.center; // fallback to the center of the spawn zone if no valid point is found
+        
+        return Vector3.positiveInfinity;
     }
 
     Vector3 RandomSpawnPoint(Bounds _bounds)
@@ -106,10 +111,13 @@ public class ObjectSpawner : MonoBehaviour
             for (int i = 0; i < Random.Range(spawnableObject.objectAmount.x, spawnableObject.objectAmount.y + 1); i++)
             {
                 Vector3 spawnPoint = RandomPointInZone(objectSpawnZone.bounds);
+                
+                if(spawnPoint.magnitude > 1000f)
+                    continue;
 
                 spawnedObjects.Add(Instantiate(spawnableObject.objectPrefab, spawnPoint, Quaternion.Euler(0, 0, Random.Range(0f, 360f))));
 
-                yield return null;
+                yield return new WaitForSeconds(0.1f);
             }
         }
     }
@@ -125,7 +133,45 @@ public class ObjectSpawner : MonoBehaviour
 
     public void RespawnSpecificObject(GameObject _object)
     {
-        _object.transform.position = RandomPointInZone(objectSpawnZone.bounds);
+        Vector3 respawnPoint = RandomPointAvoidWinZones(objectSpawnZone.bounds);
+        respawnPoint.y = objectSpawnZone.bounds.min.y;
+        
+        _object.transform.position = respawnPoint;
         _object.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+    
+    Vector3 RandomPointAvoidWinZones(Bounds _bounds)
+    {
+        WinZone[] winZones = FindObjectsOfType<WinZone>();
+        List<float> winZoneXPositions = new List<float>();
+        
+        foreach (var winZone in winZones)
+            winZoneXPositions.Add(winZone.transform.position.x);
+
+        Vector3 spawnPoint = RandomSpawnPoint(_bounds);
+        
+        int attempts = 0;
+        int winZonesSuccessful = 0;
+        float tolerance = 0.8f;
+        
+        while (attempts < maxPositionFindAttempts)
+        {
+            foreach (var winZoneX in winZoneXPositions)
+            {
+                if (spawnPoint.x < winZoneX - tolerance || spawnPoint.x > winZoneX + tolerance)
+                    winZonesSuccessful++;
+            }
+
+            if (winZonesSuccessful == winZoneXPositions.Count)
+                return spawnPoint;
+            
+            winZonesSuccessful = 0;
+            spawnPoint = RandomSpawnPoint(_bounds);
+
+            attempts++;
+        }
+
+        Debug.LogWarning("Failed to find a valid spawn point after " + maxPositionFindAttempts + " attempts.");
+        return _bounds.center; // fallback to the center of the spawn zone if no valid point is found
     }
 }
