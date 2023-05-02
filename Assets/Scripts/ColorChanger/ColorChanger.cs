9659039fed9f77a23e281c0d3f2dc2bece0c3c08
Leonie Straßer <leonie.strasser@yahoo.de>
2023-05-02 13:09:44 +0200

    ColorManagerWorks

--- Assets/Scripts/ColorChanger/ColorChanger.cs
+++ Assets/Scripts/ColorChanger/ColorChanger.cs
@@ -9,20 +9,20 @@ public class ColorChanger : MonoBehaviour
     [SerializeField] ColorPaletteSetup currentSetup;
 
 
-    [BoxGroup("Materialgroups to change Color")] [SerializeField] CustomizableMaterial stoneColorChangeMats;
+    [BoxGroup("Materialgroups to change Color")] [SerializeField] CustomizableMaterial[] ColorChangeMatsOfType;
 
     [Button]
-    void LodeColorsInScene(ColorPaletteSetup _currentPalette)
+    void TestLodeColorsFromPalette()
     {
         // für jedes Material das CustomizableMaterial x hat to folgendes
-        foreach (var mat in stoneColorChangeMats.materials)
+        foreach (var mat in ColorChangeMatsOfType[0].materials)
         {
             //Check jeden Value von X´s Colorvalue Liste 
-            foreach (var myColorVal in stoneColorChangeMats.colorValues)
+            foreach (var myColorVal in ColorChangeMatsOfType[0].colorValues)
             {
                 string _nameToCompare = myColorVal.valueName;
                 // Wenn die _currentPalette einen ColorValue mit dem selben string hat, setze hier im Colorvalue die Farbe von _currentPallettes entsprechendem ColorValue ein
-                foreach (var set in _currentPalette.allColorValueSets)
+                foreach (var set in currentSetup.allColorValueSets)
                 {
                     // Geh jedes Set des scriptable Objects durch
                     foreach (var colorVal in set.allColorValues)
@@ -38,5 +38,53 @@ public class ColorChanger : MonoBehaviour
         }
     }
 
+
+    [Button]
+    public void SetRandomColorsInScene()
+    {
+        SetRandomColorSet();
+        LodeColorsFromPalette(currentSetup);
+    }
+
+    void SetRandomColorSet()
+    {
+        int _randomListIndex = Random.Range(0, allColorSetups.Length);
+        currentSetup = allColorSetups[_randomListIndex];
+    }
+
+    void LodeColorsFromPalette(ColorPaletteSetup _currentPalette)
+    {
+        // Für jedes ColorValueSetForOneMaterial
+        foreach (var matType in ColorChangeMatsOfType)
+        {
+            // für jedes Material das ColorValueSetForOneMaterial x hat do folgendes -- Denn jedes Material braucht das selebe Color Update
+            foreach (var mat in ColorChangeMatsOfType[0].materials)
+            {
+                //Check jeden Value der Colorvalue Liste im ColorValueSetForOneMaterial
+                foreach (var myColorVal in ColorChangeMatsOfType[0].colorValues)
+                {
+                    string _nameToCompare = myColorVal.valueName;
+                    // Wenn die _currentPalette einen ColorValue mit dem selben string hat, setze hier im Colorvalue des materials die Farbe von _currentPallettes entsprechendem ColorValue ein
+                    foreach (var set in _currentPalette.allColorValueSets)
+                    {
+                        // Geh jedes Set des scriptable Objects durch -- Die Sets existieren nur um die Farben einfacher einzustellen
+                        foreach (var colorVal in set.allColorValues)
+                        {
+                            if (colorVal.valueName == _nameToCompare)
+                            {
+                                myColorVal.color = colorVal.color;
+                                SetNewColor(mat, _nameToCompare, myColorVal.color);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void SetNewColor(Material _material, string _changeColorName, Color _newColor)
+    {
+        _material.SetColor(_changeColorName, _newColor);
+    }
 
 }
